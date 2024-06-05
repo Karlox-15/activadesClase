85bfcb0b65a7c51f947bba265c89aebf2246505b
karlox-15 <cpgalvis15@gmail.com>
2024-06-04 22:02:10 -0500

    fundamentos JS

---
+++ javascript/conceptosBasicos/scripts/conceptosBasicos.js
@@ -0,0 +1,47 @@
+// Lo primero es validar que la vinculacion es correcta
+// enviamos un mensaje para validar 
+
+console.log('HOLA! MUNDO - Estoy vinculado con mi html');
+
+// variable js
+
+/* definicion ->espacio de memoria-> cajita que me guarda informacion 
+en forma de datos->diferentes tipos, numericos(2, 2.3, 4.65487465874) 
+boleabnos,arreglos,texto(strings),objetos */
+
+// definimos para usarlo constantemente
+
+/*
+
+Variables en JS
+(NO SE USA)
+1. VAR -> nos guarda datos que pueden cambiar en el tiempo (en desuso)
+2. LET -> nos guarda datos que pueden cambiar en el tiempo
+3. CONST -> Guarda datos constantes no cambian en el tiempo
+*/
+
+// 2. DECLARACION DE VARIABLES
+
+
+let nombreVariable = 'nombre';
+
+// numericos
+
+let numero = 0;
+const pi = 3.14;
+
+// texto > Strings
+
+let texto = 'hola';
+const nombre = 'carlos';
+
+// booleanos -> verdadero o falso
+
+let esVeradero = true;
+const esFalso = false;
+
+// Arreglos -> guardar as de un dato -> del mismo tipo
+// se reconocen por el uso [] y los datos se separan por comas
+
+let arreglo = [5, 6, 7, 3, 9]
+const arregloNombres = ['Carlos', 'manuela', 'Daniel']
