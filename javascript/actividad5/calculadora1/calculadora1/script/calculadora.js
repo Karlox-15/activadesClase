1e596bfe8e31d13e7d5ffe003d6b7bd1c5a96a3e
karlox-15 <cpgalvis15@gmail.com>
2024-07-10 08:59:06 -0500

    Calculadora Prueba

---
+++ javascript/actividad5/calculadora1/calculadora1/script/calculadora.js
@@ -0,0 +1,19 @@
+console.log('hola,estoy funcionando');
+let numero1;
+let numero2;
+let pantalla;
+
+numero1 = document.getElementById('numero1').value;
+numero2 = document.getElementById('numero2').value;
+pantalla = document.getElementById('pantalla');
+
+
+
+function suma() {
+
+    numero1 = document.getElementById('numero1').value;
+    numero2 = document.getElementById('numero2').value;
+
+    console.log(numero1, numero2, pantalla);
+
+}
